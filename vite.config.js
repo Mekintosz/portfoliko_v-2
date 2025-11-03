1fa4f97094bc3c2e5905e994eb6bde4101c1b108
Mekintosz <134439256+Mekintosz@users.noreply.github.com>
2025-11-03 22:41:29 +0100

    Add live wiev and github buttons to projects

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,8 +11,17 @@ export default defineConfig({
     outDir: "dist",
     assetsDir: "assets",
     sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        "black-olives": resolve(__dirname, "projects/black-olives.html"),
+        Gotujto: resolve(__dirname, "projects/Gotujto.html"),
+        "personal-blog": resolve(__dirname, "projects/personal-blog.html"),
+        "task-manager-app": resolve(
+          __dirname,
+          "projects/task-manager-app.html"
+        ),
+      },
+    },
   },
 });
-
-
-
